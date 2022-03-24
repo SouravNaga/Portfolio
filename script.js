91c7cb7a416eaa1c48652a8691ac6382ba4eb460
SouravNaga <souravnaga97@gmail.com>
2022-03-24 20:44:19 +0530

    sec commit

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
-$(document).ready(function(){
-
-  $('#menu').click(function(){
-      $(this).toggleClass('fa-times');
-      $('.navbar').toggleClass('nav-toggle');
+$(document).ready(function () {
+  $("#menu").click(function () {
+    $(this).toggleClass("fa-times");
+    $(".navbar").toggleClass("nav-toggle");
   });
 
-  $(window).on('scroll load',function(){
-    $('#menu').removeClass('fa-times');
-    $('.navbar').removeClass('nav-toggle');
+  $(window).on("scroll load", function () {
+    $("#menu").removeClass("fa-times");
+    $(".navbar").removeClass("nav-toggle");
   });
 
-  $('.portfolio .button-container .btn').click(function(){
+  $(".portfolio .button-container .btn").click(function () {
+    let filter = $(this).attr("data-filter");
 
-    let filter = $(this).attr('data-filter');
-
-    if(filter == 'all'){
-      $('.portfolio .image-container .box').show('400')
-    }else{
-      $('.portfolio .image-container .box').not('.'+filter).hide('200');
-      $('.portfolio .image-container .box').filter('.'+filter).show('400');
+    if (filter == "all") {
+      $(".portfolio .image-container .box").show("400");
+    } else {
+      $(".portfolio .image-container .box")
+        .not("." + filter)
+        .hide("200");
+      $(".portfolio .image-container .box")
+        .filter("." + filter)
+        .show("400");
     }
-
   });
 
-  $('#theme-toggler').click(function(){
-    $(this).toggleClass('fa-sun');
-    $('body').toggleClass('dark-theme');
+  $("#theme-toggler").click(function () {
+    $(this).toggleClass("fa-sun");
+    $("body").toggleClass("dark-theme");
   });
 
-  // smooth scrolling 
+  // smooth scrolling
 
-  $('a[href*="#"]').on('click',function(e){
-
+  $('a[href*="#"]').on("click", function (e) {
     e.preventDefault();
 
-    $('html, body').animate({
-
-      scrollTop : $($(this).attr('href')).offset().top,
-
-    },
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top,
+      },
       500,
-      'linear'
+      "linear"
     );
-
   });
-
 });
